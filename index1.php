6b9222da260a70c1d80d71519e0890d5cb9d3d95
solisnavarro <dikey93solis@gmail.com>
2020-11-12 17:23:08 -0500

    subiendo php

---
+++ index1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Welcome to my Page</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+<head>
+<!-- Contenido de 1 columna -->
+    <div class="row">
+       <div class="col">
+
+	      <h1>Computación e Informática</h1>
+	      <p>Actividad profesional destinada al planeamiento, instalación, configuración, administración y mantenimiento de los sistemas de información de una organización, a partir del análisis de sus requerimientos y teniendo en cuenta los criterios de calidad y seguridad establecidos.</p>
+       </div>
+	</div>
+	
+</body>
+</html>
